95c77875fac054d042e3b4f3d325b0deda65b381
Michael Murray <v823061@global.ual.com>
2020-01-16 14:59:50 -0600

    Add project files.

---
+++ AggregateFunctionLINQ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggregateFunctionLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] states = { "Illinois", "Wisconsin", "Indiana", "Iowa", "Kentucky" };
+
+
+
+            //string result = states.Aggregate((a, b) => a + " * " + b);
+
+
+            // a or accumulator is what stores the values that are aggregated from the aggregate function, 
+            // b or builder is the next item to concatonate to the accumulator.
+
+            string result = states.Aggregate((accumulator, builder) => accumulator + " * " + builder);
+
+
+
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
